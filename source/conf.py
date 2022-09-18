943b784f818bbf531b9dd678b7378050e7db6997
liuyumin <960069012@qq.com>
2022-09-18 16:06:52 +0800

    更改主题

--- source/conf.py
+++ source/conf.py
@@ -24,6 +24,6 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'sphinx_rtd_theme'
-html_theme = 'furo'
+html_theme = 'sphinx_rtd_theme'
+#html_theme = 'furo'
 html_static_path = ['_static']
