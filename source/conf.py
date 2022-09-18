71894bdab6d0f57f412ca52db077cfd6e82bd523
liuyumin <960069012@qq.com>
2022-09-18 18:24:56 +0800

    修改py

--- source/conf.py
+++ source/conf.py
@@ -27,3 +27,16 @@ language = 'zh_CN'
 html_theme = 'sphinx_rtd_theme'
 #html_theme = 'furo'
 html_static_path = ['_static']
+# At the bottom of conf.py
+
+import recommonmark
+from recommonmark.transform import AutoStructify
+
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
+            }, True)
+    app.add_transform(AutoStructify)
+
+# At the bottom of conf.py
