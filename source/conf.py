7fb62965fd3bd9815582ea1ea29bd47b4a6651e5
lym <960069012@qq.com>
2022-11-16 22:48:54 +0800

    测试

--- source/conf.py
+++ source/conf.py
@@ -1,31 +1,50 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
-project = 'Cole‘s documentation'
+project = 'mvsilicon documentation'
 copyright = '2022, By LiuYuMin'
 author = 'lym'
-release = 'v0.0.0.1'
+release = 'v0.0.0.2'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# -- General configuration ---------------------------------------------------
 
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'recommonmark',
+    'sphinx_markdown_tables'
+]
+
+source_suffix = {
+    '.rst': 'restructuredtext',
+}
+
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
-exclude_patterns = []
 
-language = 'zh_CN'
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+language = 'zh_en'
 
-html_theme = 'sphinx_rtd_theme'
+
+# html_theme = 'sphinx_rtd_theme'
 #html_theme = 'furo'
+html_theme = 'renku'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+html_logo = 'favicon.png'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
 # At the bottom of conf.py
 
 # import recommonmark
--- source/index.rst
+++ source/index.rst
@@ -3,17 +3,15 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-欢迎访问Cole的文档!
-====================================
+测试
+=====================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+   
+=======================
 
-
-
-测试项目：
-==================
 
 * :ref:`genindex`
 * :ref:`modindex`
